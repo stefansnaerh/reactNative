867025b792ac34735c393fcea3e1353ca18e1bd3
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-05-01 19:20:46 +0000

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import classPhoto from './assets/classImage.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headline}>Hello Vefskóli!</Text>
+      <Text style={styles.paragraph}>Thanks for a great school year and a awesome trip to Las Palmas! </Text>
+      <Image
+      style={styles.image}
+      source={classPhoto}
+      />
+      <Text style={styles.paragraph}>Good luck on your future endeavors!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    gap: 20,
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headline: {
+    fontSize: 32,
+  },
+  paragraph: {
+    fontSize: 20,
+    width: "80%",
+    textAlign: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 5,
   },
 });
---
+++ readme.md
@@ -0,0 +1,7 @@
+### Module 6 Javascript/React - Beyond the Browser
+
+It´s a simple project but should meet the minimum requirements for this guide.
+
+To view this app you need to have Expo Go installed. Scan the qr code with your camera and take a look!
+
+![](./assets/2DC659D1-6D48-4E35-A78C-C63B4BA1BBAF.jpeg)
